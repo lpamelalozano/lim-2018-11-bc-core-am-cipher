01daae07aa4413088d0029830f3230359bc25914
lpamelalozano <lpamelalozano@gmail.com>
2018-11-26 09:55:17 -0500

    Hacker edition: Cipher / decode other characters (numbers)

--- src/cipher.js
+++ src/cipher.js
@@ -2,9 +2,9 @@ window.cipher = {
   encode: (offSet,string)=>{
     let show = '';
     let asciiCode;
-    let convertedLetter;
+    let convertMessage;
 
-    //let convertedLetter = 0;
+    //let convertMessage = 0;
 
     for (let i=0; i < string.length; i++){
 
@@ -12,14 +12,17 @@ window.cipher = {
 
   //mayusculas
     if (asciiCode >=65 && asciiCode <=90){
-      convertedLetter= ((asciiCode - 65 + offSet) %26+65);
-      show+=String.fromCharCode(convertedLetter);
+      convertMessage = ((asciiCode - 65 + offSet) %26+65);
+      show+=String.fromCharCode(convertMessage);
 
   //minusculas
     }else if (asciiCode >=97 && asciiCode <=122) {
-      convertedLetter = (asciiCode - 97 + offSet)%26+97;
-      show+=String.fromCharCode(convertedLetter);
-
+      convertMessage = (asciiCode - 97 + offSet)%26+97;
+      show+=String.fromCharCode(convertMessage);
+  // numeros
+    }else if (asciiCode >=48 && asciiCode <=57) {
+      convertMessage = (asciiCode -48 + offSet)%10+48;
+      show+=String.fromCharCode(convertMessage);
   //otro caracter
     }else{
       show+=String.fromCharCode(asciiCode);
@@ -30,7 +33,7 @@ window.cipher = {
 
   decode: (offSet,string)=>{
     let show = '';
-    let convertedLetter = 0;
+    let convertMessage = 0;
     let asciiCode;
 
     for (let i=0; i < string.length; i++){
@@ -40,20 +43,23 @@ window.cipher = {
    //mayusculas
     if (asciiCode >=65 && asciiCode <=90){
 
-      convertedLetter = (asciiCode - 90 - offSet) % 26 + 90;
+      convertMessage = (asciiCode - 90 - offSet) % 26 + 90;
 
 
-      show+=String.fromCharCode(convertedLetter);
+      show+=String.fromCharCode(convertMessage);
 
 
   //minusculas
     }else if (asciiCode >=97 && asciiCode <=122) {
 
-      convertedLetter = (asciiCode - 122 - offSet) % 26 + 122;
+      convertMessage = (asciiCode - 122 - offSet) % 26 + 122;
 
 
-      show+=String.fromCharCode(convertedLetter);
+      show+=String.fromCharCode(convertMessage);
 
+    }else if (asciiCode >=48 && asciiCode <=57) {
+      convertMessage = (asciiCode -57 - offSet)%10+57;
+      show+=String.fromCharCode(convertMessage);
 
   //otro caracter
     }else{
